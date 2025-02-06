e144decc6d94feede49bda78f9d4f749e4fee08b
Violetta <violettacerkaseninova@gmail.com>
2025-02-06 17:43:21 +0100

    discount_form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 
 import React from 'react'
+import DiscountForm from '../components/discountForm/DiscountForm'
 
 function Home() {
   return (
     <div>
+    <DiscountForm/>
      <p>This is HOMEPAGE</p>
     </div>
   )
 }
+
 
 export default Home
