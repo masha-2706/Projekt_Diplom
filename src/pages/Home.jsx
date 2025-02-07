7f52e56eb9b0a25d27c92bd3538719b5037ca2d0
Violetta <violettacerkaseninova@gmail.com>
2025-02-07 17:05:29 +0100

    центрировала body + исправила margins and paddings

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import BannerHome from "../components/bannerHome/BannerHome";
 import DiscountForm from "../components/discountForm/DiscountForm";
 function Home() {
   return (
-    <div style={{ width: '1440px', backgroundColor: '#fffff1' }}>
+    <div style={{ width: '1440px', backgroundColor: '#fffff1' }}> {/*мешает адаптиву style*/}
       <BannerHome />
       <ContentBlock title="Categories" quantity={4} type={'сategories'} navigationButtonLabel={'All categories'} />
       <DiscountForm />
