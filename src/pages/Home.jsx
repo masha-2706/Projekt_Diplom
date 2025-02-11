d30733099b9a3c89c4ad805b0cbc6462dffdd488
Maria <maria.malysheva.87@gmail.com>
2025-02-12 00:36:57 +0100

    корректировки адаптивности Home Page. добавила в contentBlock max-width: 1360px; добавила в NavMenu.module.css  hover центрировала блок Footer установила в App.css max-width для тега body

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-
 import React from 'react'
 import ContentBlock from '../components/ContentBlock/ContentBlock'
 import BannerHome from "../components/bannerHome/BannerHome";
 import DiscountForm from "../components/discountForm/DiscountForm";
+
+
 function Home() {
   return (
-    <div style={{ width: '1440px', backgroundColor: '#fffff1' }}> {/*мешает адаптиву style*/}
+    <div>
       <BannerHome />
       <ContentBlock title="Categories" quantity={4} type={'сategories'} navigationButtonLabel={'All categories'} />
       <DiscountForm />
