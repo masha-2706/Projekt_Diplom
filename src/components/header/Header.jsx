44dfe6a3aafb8182160747bd1c8d509cd9a52c2b
YanaKashaba <missyasua@gmail.com>
2025-02-11 17:43:21 +0100

    fix structur componentn header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,32 +1,26 @@
-import React from 'react'
-import NavMenu from '../navMenu/NavMenu'
-import ThemeBtn from './components/themeBtn/ThemeBtn'
-import BlockIcons from './components/blockIcons/BlockIcons'
-import s from './Header.module.css'
-import Container from '../container/Container'
+import React from "react";
+import NavMenu from "../navMenu/NavMenu";
+import ThemeBtn from "../ui/themeBtn/ThemeBtn";
+import BlockIcons from "../ui/blockIcons/BlockIcons";
+import s from "./Header.module.css";
+
 
 // Это наш Header в котором мы распологаем :
 export default function Header() {
   return (
-    <Container>
-
-   <header className={s.header}>
-    <div className={s.logoContainer}>
-    <img className={s.logo} src="./logo/logo.png" alt="logo"/> 
-    {/* логотип */}
-    <ThemeBtn/>
-    {/* кнопка для изменения темы (день /ночь) небходимы функции для переключения для этого необходимо ( в компонент icon кот находит в ui) необходимо передать  classname , id ) id -передано интерполяцией  */}
-    </div>
-    <NavMenu/>
-    {/* навигация по сайту  */}
-    <BlockIcons/>
-    {/* блок иконок (лайк и корзина ) */}
-
-
-    
-   </header>
-
-   </Container>
    
-  )
+      <header className={s.header}>
+        <div className={s.logoContainer}>
+          <img className={s.logo} src="./logo/logo.png" alt="logo" />
+          {/* логотип */}
+          <ThemeBtn />
+          {/* кнопка для изменения темы (день /ночь) небходимы функции для переключения для этого необходимо ( в компонент icon кот находит в ui) необходимо передать  classname , id ) id -передано интерполяцией  */}
+        </div>
+        <NavMenu />
+        {/* навигация по сайту  */}
+        <BlockIcons />
+        {/* блок иконок (лайк и корзина ) */}
+      </header>
+   
+  );
 }
---
+++ src/components/ui/themeBtn/ThemeBtn.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Icon from "../Icon";
+import s from "./ThemeBtn.module.css";
+
+export default function ThemeBtn() {
+  return (
+    <div className={s.themeToggle}>
+      <Icon className={s.iconSun} id="icon-brightness" w={20} h={20} />
+      <Icon className={s.iconMoon} id="icon-sleep" w={20} h={20} />
+      <input type="checkbox" id="theme-switch" className={s.themeSwitch} />
+      <label htmlFor="theme-switch" className={s.btn}></label>
+    </div>
+  );
+}
