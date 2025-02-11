67beb0665e6e59e02ee2adfbc5fb98093dec9932
Maria <maria.malysheva.87@gmail.com>
2025-02-11 22:23:21 +0100

    сделала телефон и иконки whatsapp and instagramm кликабельными убрала border у карты исправила border-radius у блоков перенесла изображения в public/media/footerImages

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,13 @@
 import style from "./Footer.module.css";
-import iconInstagramm from "../../media/icons/ic-instagram.svg";
-import iconWhatsapp from "../../media/icons/ic-whatsapp.svg";
 
 function Footer() {
-  {/* url адреса на /Google map вынесен в константу */}
+  // url адреса на Google Maps вынесен в константу
   const mapAdress =
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.4089985786927!2d13.372748476191973!3d52.50793693712319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a851cbdeaf3909%3A0xe189201578bb3535!2sLinkstra%C3%9Fe%202%2FOG%2C%2010%2C%2010785%20Berlin!5e0!3m2!1sde!2sde!4v1739045210090!5m2!1sde!2sde";
+  const instagramLink = "https://www.instagram.com/"; // url адреса на Instagram вынесен в константу
+  const whatsappLink = "https://www.whatsapp.com/"; // url адреса на Whatsapp вынесен в константу
+  const iconInstagramm = "/media/footerImages/ic-instagram.svg"; // Изображение icon Instagram
+  const iconWhatsapp = "/media/footerImages/ic-whatsapp.svg"; // Изображение icon Whatsapp
 
   return (
     <div className={style.footer_wrapper}>
@@ -14,13 +16,22 @@ function Footer() {
       <div className={style.footer_container}>
         <div className={style.footer_contact_phone}>
           <p className={style.footer_contact_title}>Phone</p>
-          <p className={style.footer_contact_description}>+49 999 999 99 99</p>
+          <a
+            href="tel:+49 999 999 99 99"
+            className={style.footer_contact_description}
+          >
+            +49 999 999 99 99
+          </a>
         </div>
 
         <div className={style.footer_contact_socials}>
           <p className={style.footer_contact_title}>Socials</p>
-          <img src={iconInstagramm} alt="#" />
-          <img src={iconWhatsapp} alt="#" />
+          <a href={instagramLink}>
+            <img src={iconInstagramm} alt="instagram" />
+          </a>
+          <a href={whatsappLink}>
+            <img src={iconWhatsapp} alt="whatsapp" />
+          </a>
         </div>
 
         <div className={style.footer_contact_address}>
---
+++ src/components/ui/blockIcons/BlockIcons.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Icon from '../Icon'
+import s from './BlockIcons.module.css'
+
+export default function BlockIcons() {
+  return (
+    <div className={s.container}> 
+        <Icon className={s.heart} id="icon-hert" w={44} h={44} />
+        <Icon className={s.cart} id="icon-cart" w={44} h={44} />
+    </div>
+  )
+}
