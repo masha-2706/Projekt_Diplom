1e6001d7afb5c201e013734f1ceb5a1ae2ad14ed
Maria <maria.malysheva.87@gmail.com>
2025-02-13 15:58:41 +0100

    в компоненте NavMenu заменет импорт кнопки на актуальный

--- src/components/navMenu/NavMenu.jsx
+++ src/components/navMenu/NavMenu.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import s from './NavMenu.module.css'
-import GreenButton from '../ui/GreenButton'
+import GreenButton from '../ui/Button.jsx'
 
 
 function NavMenu() {
