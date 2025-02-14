30710cd2580d3e269df92ff835286f8b0c7f61a6
YanaKashaba <missyasua@gmail.com>
2025-02-14 13:36:32 +0100

    add link to logo and diskount button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,32 +1,33 @@
-import React from 'react'
-import NavMenu from '../navMenu/NavMenu'
-import ThemeBtn from './components/themeBtn/ThemeBtn'
-import BlockIcons from './components/blockIcons/BlockIcons'
-import s from './Header.module.css'
-import Container from '../container/Container'
+import React from "react";
+import NavMenu from "../navMenu/NavMenu";
+import ThemeBtn from "../button/Button";
+import BlockIcons from './components/blockIcons/BlockIcons';
+import s from "./Header.module.css";
+import { NavLink } from "react-router";
 
 // Это наш Header в котором мы распологаем :
 export default function Header() {
   return (
-    <Container>
+    <header className={s.header}>
+      <div className={s.logoContainer}>
+        {/* Обернули логотип в нав линк для обеспечения навигации при клике на линк */}
+        <NavLink className={s.link} to="/">
+          <img className={s.logo} src="./logo/logo.png" alt="logo" />
+        </NavLink>
 
-   <header className={s.header}>
-    <div className={s.logoContainer}>
-    <img className={s.logo} src="./logo/logo.png" alt="logo"/> 
-    {/* логотип */}
-    <ThemeBtn/>
-    {/* кнопка для изменения темы (день /ночь) небходимы функции для переключения для этого необходимо ( в компонент icon кот находит в ui) необходимо передать  classname , id ) id -передано интерполяцией  */}
-    </div>
-    <NavMenu/>
-    {/* навигация по сайту  */}
-    <BlockIcons/>
-    {/* блок иконок (лайк и корзина ) */}
-
-
-    
-   </header>
-
-   </Container>
-   
-  )
+        {/* логотип */}
+        <ThemeBtn />
+        {/* кнопка для изменения темы (день /ночь) небходимы функции для переключения для этого необходимо ( в компонент icon кот находит в ui) необходимо передать  classname , id ) id -передано интерполяцией  */}
+      </div>
+      <NavMenu />
+      {/* навигация по сайту  */}
+      <BlockIcons />
+      {/* блок иконок (лайк и корзина ) */}
+    </header>
+  );
 }
+
+
+
+
+
--- src/components/navMenu/NavMenu.jsx
+++ src/components/navMenu/NavMenu.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
-import { NavLink } from 'react-router'
-import s from './NavMenu.module.css'
-import GreenButton from '../ui/GreenButton'
+import React from "react";
+import { NavLink , Link} from "react-router";
+import s from "./NavMenu.module.css";
+import GreenButton from "../ui/GreenButton";
 
 function NavMenu() {
   return (
-    <div className={s.navMenuContainer}><GreenButton text= {"1 day discount!"} className={s.btn}/>
+    <div className={s.navMenuContainer}>
+      <Link className={s.link} to="/sales">
+        <GreenButton text={"1 day discount!"} className={s.btn} />
+      </Link>
+
       <div className={s.nav}>
-       <NavLink className={s.link} to='/'>Main Page</NavLink> 
-       <NavLink className={s.link} to='/categories'>Categories</NavLink> 
-       <NavLink className={s.link} to='/products'>All products</NavLink> 
-       <NavLink className={s.link} to='/sales'>All sales</NavLink> 
-     </div>
-</div>
-  )
+        <NavLink className={s.link} to="/">
+          Main Page
+        </NavLink>
+        <NavLink className={s.link} to="/categories">
+          Categories
+        </NavLink>
+        <NavLink className={s.link} to="/products">
+          All products
+        </NavLink>
+        <NavLink className={s.link} to="/sales">
+          All sales
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
-export default NavMenu
+export default NavMenu;
