28819fa56e9d9fba54ef4988a9b611dd21de2f39
villerOrange <viller16@gmail.com>
2025-02-22 15:40:54 +0100

    refactor: replace header structure in CardsContainer with BlockTitle component

---
+++ src/components/BlockTitle/BlockTitle.jsx
@@ -0,0 +1,23 @@
+import s from './BlockTitle.module.css';
+import NavigationButton from "../ui/NavigationButton/NavigationButton";
+
+
+export default function BlockTitle({ title, navButton }) {
+    return (
+        <div className={s.CardsContainer_header}>
+            <h2>{title}</h2>
+            {/* если  navButton = true - отрисуется линия от заголовка и сам navButton */}
+            {navButton && <div className={s.CardsContainer_header_line}></div>}
+            {navButton && (
+                <div className={s.CardsContainer_header_buttons}>
+                    {title === "Categories" && (
+                        <NavigationButton text="All сategories" link="/categories" />
+                    )}
+                    {title === "Sales" && (
+                        <NavigationButton text="All sales" link="/sales" />
+                    )}
+                </div>
+            )}
+        </div>
+    )
+}
